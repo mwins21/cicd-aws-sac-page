10376f091d1f908e366ec5264ac22ce6382e77a7
mwins21 <64426103+mwins21@users.noreply.github.com>
2020-04-29 10:34:11 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ data "luminate_user" "users" {
 
 data "luminate_group" "groups" {
   identity_provider_id = data.luminate_identity_provider.idp.identity_provider_id
-  users                = [var.luminate_group]
+  groups                = [var.luminate_group]
 }
 
 // Output variables
