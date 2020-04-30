25f7d2aa9aa516de60d63d18b9f26527477f0af1
mwins21 <64426103+mwins21@users.noreply.github.com>
2020-04-30 08:54:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -142,8 +142,8 @@ data "luminate_user" "users" {
 data "luminate_group" "groups" {
   identity_provider_id = data.luminate_identity_provider.idp.identity_provider_id
   //identity_provider_id = "My-SAC-Okta"
-  //groups                = [var.luminate_group]
-  groups                = ["Developers"]
+  groups                = [var.luminate_group]
+  //groups                = ["Developers"]
 }
 
 // Output variables
